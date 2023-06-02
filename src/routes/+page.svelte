611e06f21b69c4362de70f15e3c13dacac7367f4
Sergi Camps <sergicampsgili@gmail.com>
2023-06-02 03:13:01 +0200

    tailwind setup & first home-page prototype

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>2 T E C</title>
+</svelte:head>
+<div class="flex justify-center items-center w-screen h-screen bg-black overflow-hidden">
+	<div>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+		<h1 class="text-white text-9xl font-inter">2TEC</h1>
+	</div>
+</div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
