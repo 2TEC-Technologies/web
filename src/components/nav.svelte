6c0a6b9b6cdbe59f9ef2b05cfebdabe5041e7e5e
Sergi Camps <sergicampsgili@gmail.com>
2023-06-07 20:28:04 +0200

    First commit fater deleting original branch
    Deleted original branch, i had big problems with commits and it was a mess

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,10 +1,5 @@
 <div class="flex fixed">
-    <div class="bg-glass-grey backdrop-blur-xl fixed border-slate-400 w-screen p-4 border-b"
-    
-    >
-
-        <a class="text-slate-50" href="/about">About</a>
-
-    </div>
+	<div class="bg-glass-grey backdrop-blur-xl fixed border-slate-400 w-screen p-4 border-b">
+		<a class="text-slate-50" href="/about">About</a>
+	</div>
 </div>
-
