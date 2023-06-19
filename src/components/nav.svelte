765a9b594367bf96c120602b9b7e2a1717792704
Sidney Silva <67187745+sxdny@users.noreply.github.com>
2023-06-19 21:02:55 +0200

    navbar update

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,13 +1,25 @@
-<div class="flex fixed nav">
+<div class="flex fixed nav font-sans">
     <div
-        class="flex justify-around backdrop-blur-md  bg-dark-grey fixed border-slate-400 w-[95%] p-3 border top-5 left-1/2 translate-x-[-50%] rounded-full items-center"
+        class="flex justify-around backdrop-blur-md  bg-dark-grey fixed border-slate-400 w-[95%] nav border top-5 left-1/2 translate-x-[-50%] rounded-full items-center"
     >
         <div class="logo">
-            <a class="logo text-slate-50 font-medium text-lg" href="/">2TEC</a>
+            <a class="logo transition text-slate-50 font-medium text-[16px]" href="/">2TEC</a>
         </div>
-        <div class="buttons flex justify-end gap-x-8 w-3/4">
-            <a class="text-white-smooth text-[14px] font-normal" href="/about">About</a>
-            <a class="text-white-smooth text-[14px] font-normal" href="/contact">Contact</a>
+        <div class="buttons flex justify-end gap-x-8 w-3/4 items-center">
+            <a class="text-smooth-gray transition text-[14px] font-normal" href="/about">About</a>
+            <a class="contact rounded-md bg-yellow-vivid text-dark-grey text-[14px] text-gray" href="/contact">Contact</a>
         </div>
     </div>
 </div>
+
+<style>
+    .nav {
+        padding: 12px 32px;
+    }
+
+    
+
+    .contact {
+        padding: 6px 12px;
+    }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ export default {
 				'yellow-vivid': '#E2E701',
 				'blur-grey' : 'rgba(16, 16, 16, 0.15)',
 				'dark-grey' : '#1D1D1FB3',
-				'white-smooth' : '#F5F5F7'
+				'white-smooth' : '#F5F5F7',
+				'gray' : '#343434',
+				'smooth-gray' : '#D1D1D1'
 			},
 			backgroundImage: {
 				'about-wallpaper': 'url("/wallpaper-about.jpg")'
